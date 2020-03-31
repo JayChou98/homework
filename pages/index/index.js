9bcc5207ebd628910490f23c18ed6ae60d324c65
ChangAnMou-rba <1414681749@qq.com>
2020-03-31 20:09:19 +0800

    modify

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,17 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  
+  onLoad(){
+    wx.request({
+      url: 'https://route.showapi.com/1700-1?showapi_appid=167149&showapi_sign=f8b9f9cd6f9747b9ab6e16a06cf2bcc1',
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          list:res.data.showapi_res_body.storylist
+        })
+      },
+      fail(err){
+        console.log(err)
+      }
+    })
+  }
 })
