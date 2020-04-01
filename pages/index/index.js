7948f4252e7ec9dda2d0cd6d59f460249ba6fd9e
ChangAnMou-rba <1414681749@qq.com>
2020-04-01 20:50:41 +0800

    modify

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,47 @@
 //index.js
+//获取应用实例
+const app = getApp()
 Page({
+  data:{
+    page:1
+  },
   onLoad(){
+    var url='https://route.showapi.com/87-60?showapi_appid=167149&showapi_sign=f8b9f9cd6f9747b9ab6e16a06cf2bcc1&provinceName=北京';
+    this.setData({
+      url:url
+    })
+    wx.showLoading({
+      title: '加载中'
+    })
     wx.request({
-      url: 'https://route.showapi.com/1700-1?showapi_appid=167149&showapi_sign=f8b9f9cd6f9747b9ab6e16a06cf2bcc1',
-      success:(res)=>{
-        console.log(res);
-        this.setData({
-          list:res.data.showapi_res_body.storylist
-        })
-      },
-      fail(err){
-        console.log(err)
+      url:url,
+      success:res=>{
+       this.setData({
+         hospitalList:res.data.showapi_res_body.hospitalList
+       })
+       wx.hideLoading({})
       }
+    })
+  },
+  onReachBottom(){
+    wx.showLoading({
+      title: '加载中'
+    })
+    wx.request({
+      url: this.data.url+'&page='+(this.data.page+1),
+      success:res=>{
+        let newHospitalList=this.data.hospitalList.concat(res.data.showapi_res_body.hospitalList)
+        this.setData({
+          hospitalList:newHospitalList
+        })
+        wx.hideLoading({})
+      }
+    })
+  },
+  toDetail(e){
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/detail/detail?id='+id
     })
   }
 })
